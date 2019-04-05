52df583421f80a85605bb128da4d90e48b8b541a
Noah Rose Ledesma <roseledesma@ucdavis.edu>
2019-04-04 20:05:49 -0700

    Add resource class skeleton

---
+++ src/resource/resource.ts
@@ -0,0 +1,44 @@
+export class Resource
+{
+    name: string;
+    location: object | undefined;
+    tags: Array<string>;
+    details: object | undefined;
+
+    /**
+     * @param name The name of the resource
+     * @param location An object containing the geopoint (coordinates) and address of a resource
+     * @param tags A list of the resources 'tags' used to descrive the goods and/or services this resource offers
+     */
+    constructor( name: string, location: object, tags: Array<string> )
+    {
+        this.name = name;
+        this.tags = tags;
+
+        if( location.hasOwnProperty("geopoint") || location.hasOwnProperty("address") )
+        {
+            this.location = location;
+        }
+        else
+        {
+            if( location != undefined )
+            {
+                // Something has gone wrong. TODO: Handle this error
+            }
+            this.location = undefined;
+        }
+        this.details = undefined;
+    }
+
+    /**
+     *  Retrieves details about this resource from the database
+     *  Must be called before any info paragraph, contact info, operating hours, and any other details are accessed
+     */
+    public getDetails() {
+        if( this.details !== undefined )
+        {
+            // TODO: Perhaps this should invoke a warning?
+            return;
+        }
+    }
+}
