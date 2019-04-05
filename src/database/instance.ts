4c84bcf14988393e1944115d5cc7e85049815ed3
Noah Rose Ledesma <roseledesma@ucdavis.edu>
2019-04-05 09:24:43 -0700

    Add database singleton

---
+++ src/database/instance.ts
@@ -0,0 +1,30 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import {FirebaseFirestore} from '@firebase/firestore-types';
+
+export const Database = (function(){
+
+    const FIREBASE_CONFIG = {
+        apiKey: "AIzaSyA2UDNIbN1dQ_RS_Pc2Y0LfZa7_l8i3-Rc",
+        authDomain: "nan-tech-displacement-database.firebaseapp.com",
+        databaseURL: "https://nan-tech-displacement-database.firebaseio.com",
+        projectId: "nan-tech-displacement-database",
+        storageBucket: "nan-tech-displacement-database.appspot.com",
+        messagingSenderId: "126845761108"
+    };
+
+    let instance: FirebaseFirestore;
+    function createInstance()
+    {
+        firebase.initializeApp( FIREBASE_CONFIG );
+        instance = firebase.firestore();
+    }
+
+    return {
+        getInstance: () => {
+            if( instance === undefined )
+                createInstance();
+            return instance;
+        }
+    }
+})();
